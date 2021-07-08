2aa5cbf64911458bc16e285aee0a6377e0b9d41d
rapharlc <rapharlc@gmail.com>
2021-07-07 22:17:33 -0300

    Criaçao da Classe Pessoa

--- src/classes/Pessoa.java
+++ src/classes/Pessoa.java
@@ -1,5 +1,37 @@
 package classes;
 
 public class Pessoa {
-    
+public String nome;
+public String sobrenome;
+public String telefone;
+
+
+    public String getNome() {
+       return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getTelefone() {
+        return telefone
+    ;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    } 
+    public Pessoa(){
+
+    }   
+    public Pessoa(String nome, String sobrenome, String contato){
+        this.setNome(nome);
+        this.setSobrenome(sobrenome);
+        this.setTelefone(telefone);
+
+    }
 }
